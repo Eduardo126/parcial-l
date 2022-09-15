57fe7c16b6ce0d12e0b5cfb7f8f1b14c53e4add8
eduardo <tato87911@gmail.com>
2022-09-14 21:37:43 -0600

    Agregar archivos de proyecto.

---
+++ parcial l/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace parcial_l
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el numero de estudiantes: ");
+            int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            for () ;
+            {
+                Console.Write("Nombre: ");
+                Console.ReadLine();
+                Console.Write("Apellido:");
+                Console.ReadLine();
+                Console.Write("Primer nota: ");
+                double.Parse(Console.ReadLine());
+                Console.Write("Segunda nota: ");
+                double.Parse(Console.ReadLine());
+                Console.Write("Terca nota: ");
+                double.Parse(Console.ReadLine());
+            }
+            public class Student
+        {
+
+            private string _Name;
+            private string _namee;
+            private double _nota1;
+            private double _nota2;
+            private double _nota3;
+        }
+        public string Name
+        {
+            get => _Name;
+            set => _Name = value;
+        }
+        public string _ape
+        {
+            get => _ape;
+            set => _ape = value;
+        }
+        public double nota1
+        {
+            get => _nota1;
+            set => _nota1 = value;
+        }
+        public double nota2
+        {
+            get => _nota2;
+            set => _nota2 = value;
+        }
+        public double grade3
+        {
+            get => _nota3;
+            set => _nota3 = value;
+        }
+    }
+    }
+}
